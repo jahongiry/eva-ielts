43a1484df546531cadd46c56f0f4bdc39e527e5e
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2024-05-04 14:36:31 +0500

    Add host

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,5 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+  config.hosts << "web-production-71d1d.up.railway.app"
 end
